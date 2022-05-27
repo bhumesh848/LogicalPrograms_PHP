b8cce519168eaa880898575e7b9df2dc4e4c1323
unknown <bhumesh.ranjaneb598@gmail.com>
2022-05-27 14:54:57 +0530

    Added:Logical Programs

---
+++ FibonacciSeries.php
@@ -0,0 +1,38 @@
+<?php
+class FibonacciSeries
+{
+    /**
+     * function fibonacci calculates the fibonaccci series upto nth value
+     * parameters - nth value, firstnumber, second number of series.
+     */
+    function fibonacci($nthValue)
+    {
+        $firstNum=0;
+        $secondNum=1;
+        echo $firstNum . " ";
+        echo $secondNum . " ";
+        for ($i = 0; $i <= $nthValue - 2; $i++) {
+            
+            echo $firstNum + $secondNum . " ";
+            $temp = $firstNum;
+            $firstNum = $secondNum;
+            $secondNum = $temp + $secondNum;
+        }
+    }
+}
+// Taking input from the user
+
+$nthValue = readline("How many numbers of series you want: ");
+
+/**
+ * Using if-else statement to check whether the input entered
+ * by user is valid or not
+ */
+if (is_numeric($nthValue)){        
+    $myseries = new FibonacciSeries();  //creating Object
+    $myseries->fibonacci($nthValue); //using function
+}
+else {
+    echo "PLEASE ENTER VALID INPUT"; //Invalid message 
+}
+?>
